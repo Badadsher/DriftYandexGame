5d547ab780095cb6f9d2803b583ac8015078cc00
Joe Biden <dominserv123@mail.ru>
2024-12-27 22:53:19 +0300

    Инициализация модулей сдк

--- DriftGame/Assets/Scripts/Disabler.cs
+++ DriftGame/Assets/Scripts/Disabler.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using YG;
 public class Disabler : MonoBehaviour
 {
     [SerializeField] private GameObject warnningAD;
---
+++ DriftGame/Assets/Scripts/SaveManagement/SavesYG.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YG;
+namespace YG
+{
+    public partial class SavesYG
+    {
+        // Ваши данные для сохранения
+        public int coins = 5; // Пример
+    }
+}
