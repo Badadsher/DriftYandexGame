66682dfe230b82f2e40139d038886b4767208b28
Joe Biden <dominserv123@mail.ru>
2025-01-05 01:53:06 +0300

    мелкие баги

--- DriftGame/Assets/Scripts/GameManager.cs
+++ DriftGame/Assets/Scripts/GameManager.cs
@@ -1,16 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [field: SerializeField] public LevelType type { get; private set; }
+    private Slider hpBar;
+    [SerializeField] private GameObject loseBar;
+    [SerializeField] private GameObject loadLevelUI;
     void Start()
     {
+        loadLevelUI.SetActive(true);
         if (type == LevelType.Zombie)
         {
          SaveManager.ResetKilledZombiesCount();
-        
+         hpBar = GameObject.Find("Health").GetComponent<Slider>();
+        }
+    }
+
+    public void MinusHp()
+    {
+        hpBar.value -= 10;
+        DeathChecker();
+    }
+
+    private void DeathChecker()
+    {
+        if (hpBar.value <= 0)
+        {
+            loseBar.SetActive(true);
+        }
+        else
+        {
+            return;
         }
     }
 }
--- DriftGame/Assets/Scripts/ZombieLogic.cs
+++ DriftGame/Assets/Scripts/ZombieLogic.cs
@@ -1,22 +1,22 @@
+
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using YG;
-using TMPro;
 
 public class ZombieLogic : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
     private Transform player;
     public float attackDistance = 1.5f; // Расстояние для атаки
-    public float rotationSpeed = 5f; // Скорость поворота
+    public float rotationSpeed = 10f; // Увеличенная скорость поворота
     private Animator animator;
     private Rigidbody rb; // Rigidbody для физики
     private bool isAttacking = false; // Флаг для отслеживания состояния атаки
     private ParticleSystem dyingPart;
     private TextMeshProUGUI countZombie;
-
+    private GameManager _gameManager;
     public delegate void ZombieDestroyedHandler();
     public event ZombieDestroyedHandler OnZombieDestroyed;
 
@@ -26,13 +26,9 @@ public class ZombieLogic : MonoBehaviour
         navMeshAgent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
         player = GameObject.FindWithTag("Player")?.transform;
-
-        if (player == null)
-        {
-            Debug.LogError("Player not found! Make sure the player has the tag 'Player'.");
-        }
-
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         rb = GetComponent<Rigidbody>();
+
         // Настройки NavMeshAgent
         navMeshAgent.speed = 6f; // Установите максимальную скорость
         navMeshAgent.acceleration = 20f; // Установите максимальное ускорение
@@ -47,65 +43,70 @@ public class ZombieLogic : MonoBehaviour
 
         if (navMeshAgent.enabled)
         {
+            // Если игрок далеко, зомби движется к нему
             if (distanceToPlayer > attackDistance)
             {
-                if (!isAttacking) // Проверяем, не атакует ли зомби в данный момент
-                {
-                    if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
-                    {
-                        navMeshAgent.SetDestination(player.position);
-                    }
-
-                    // Плавный поворот к игроку
-                    Vector3 direction = (player.position - transform.position).normalized;
-                    if (direction != Vector3.zero)
-                    {
-                        Quaternion lookRotation = Quaternion.LookRotation(direction);
-                        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
-                    }
-                }
+                navMeshAgent.SetDestination(player.position);
+                RotateTowardsPlayer();
             }
-            else
+            else // Если игрок в зоне атаки
             {
                 if (!isAttacking) // Проверка на состояние атаки
                 {
-                    isAttacking = true; // Устанавливаем флаг атаки
-                    navMeshAgent.speed = 0;
-                    navMeshAgent.isStopped = true; // Остановить зомби перед атакой
-                    animator.SetBool("Punching", true); // Запуск анимации удара
-                    navMeshAgent.ResetPath();
-                    StartCoroutine(AttackCooldown()); // Запускаем корутину атаки
+                    StartAttack();
                 }
             }
         }
     }
 
+    private void RotateTowardsPlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        if (direction != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    private void StartAttack()
+    {
+        isAttacking = true; // Устанавливаем флаг атаки
+        navMeshAgent.ResetPath();
+        navMeshAgent.isStopped = true; // Остановить зомби перед атакой
+        animator.SetBool("Punching", true); // Запуск анимации удара
+        StartCoroutine(AttackCooldown()); // Запускаем корутину атаки
+    }
+
     private System.Collections.IEnumerator AttackCooldown()
     {
-        yield return new WaitForSeconds(1f);
-        animator.SetBool("Punching", false); // Вернуться к состоянию ожидания или бега
-        yield return new WaitForSeconds(3f); // Задержка между атаками (например, 1 секунда)
-        isAttacking = false; // Сбрасываем флаг атаки
-        // Проверка расстояния до игрока перед возобновлением движения
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-        if (distanceToPlayer > attackDistance)
+        yield return new WaitForSeconds(1f); // Задержка перед проверкой атаки
+        float distanceToPlayerForKill = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayerForKill <= attackDistance)
         {
-            navMeshAgent.isStopped = false; // Возобновить движение после атаки
-            navMeshAgent.speed = 9;
+            _gameManager.MinusHp();
         }
+        yield return new WaitForSeconds(1f); // Задержка после удара
+        animator.SetBool("Punching", false); // Вернуться к состоянию ожидания или бега
+        yield return new WaitForSeconds(2f); // Задержка между атаками
+        isAttacking = false; // Сбрасываем флаг атаки
+        navMeshAgent.isStopped = false; // Возобновить движение после атаки
     }
 
     void OnTriggerEnter(Collider other)
     {
         PrometeoCarController prometeoCarController = other.GetComponent<PrometeoCarController>();
-        float carSpeed = prometeoCarController.carSpeed;
-        
-        if (carSpeed >= 40 && other.CompareTag("Player"))
+        if (prometeoCarController != null)
         {
-            Vector3 forceDirection = (transform.position - other.transform.position).normalized; // Направление от автомобиля
-            rb.isKinematic = false; // Убедитесь, что Rigidbody не кинематический
-            rb.AddForce(forceDirection * 100000f); // Применяем силу (настройте значение по необходимости)
-            StartCoroutine(KillZombie());
+            float carSpeed = prometeoCarController.carSpeed;
+            if (carSpeed >= 40 && other.CompareTag("Player"))
+            {
+                Vector3 forceDirection = (transform.position - other.transform.position).normalized; // Направление от автомобиля
+                rb.isKinematic = false; // Убедитесь, что Rigidbody не кинематический
+                rb.velocity = Vector3.zero; // Отключаем текущее движение перед применением силы
+                rb.AddForce(forceDirection * 500f, ForceMode.Impulse); // Применяем силу (настройте значение по необходимости)
+                StartCoroutine(KillZombie());
+            }
         }
     }
 
@@ -113,35 +114,17 @@ public class ZombieLogic : MonoBehaviour
     {
         dyingPart = transform.Find("DyingParticle").GetComponent<ParticleSystem>();
         dyingPart.Play();
-        
-        ParticleSystem.MainModule mainModule = dyingPart.main;
-        float initialLifetime = mainModule.startLifetime.constant;
 
         animator.Play("die");
-
-        SkinnedMeshRenderer skinnedMeshRenderer = transform.Find("zombie").GetComponent<SkinnedMeshRenderer>();
-        Material material = skinnedMeshRenderer.material; // Получаем материал
-        
         OnZombieDestroyed?.Invoke(); // Проверяем, есть ли подписчики и вызываем событие
-        
-        Color color = material.color; // Получаем текущий цвет материала
-        
-        for (float t = 0; t < 1; t += Time.deltaTime / 2) // 2 секунды для исчезновения
-        {
-            color.a = Mathf.Lerp(1, 0, t); // Плавно уменьшаем альфа-канал от 1 до 0
-            material.color = color; // Применяем новый цвет
-            mainModule.startColor = new ParticleSystem.MinMaxGradient(color); // Применяем новый цвет
-            
-            mainModule.startLifetime = Mathf.Lerp(initialLifetime, 0, t); // Уменьшаем lifetime
-            
-            yield return null; // Ждем один кадр
-        }
+
+        yield return new WaitForSeconds(2f); // Время анимации смерти
 
         SaveManager.SetKilledZombiesCount();
-        
+
         int zb = SaveManager.LoadKilledZombies();
         countZombie.text = zb + "/20";
-    
+
         Destroy(gameObject); // Уничтожаем объект после исчезновения
     }
 }
