49b679079cb34414b20a4de5c76751bf89e525e0
Joe Biden <48367668+Badadsher@users.noreply.github.com>
2025-04-03 10:31:26 +0300

    Добил якоря в режиме

--- DriftGame/Assets/Scripts/UI/TrainingScript.cs
+++ DriftGame/Assets/Scripts/UI/TrainingScript.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrainingScript : MonoBehaviour
 {
+    [SerializeField] private Button _buttonResume;
+
+    private void Awake()
+    {
+        _buttonResume.onClick.AddListener(StopPause);
+    }
+
     public void StartPause()
     {
         Time.timeScale = 0.0f;
